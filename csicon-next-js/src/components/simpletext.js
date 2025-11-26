891f4b306f75bd0173d34b33bd85058eae873fcf
aubreyjcook <aubreyjcook@gmail.com>
2025-11-26 10:41:32 -0600

    update

--- csicon-next-js/src/components/simpletext.js
+++ csicon-next-js/src/components/simpletext.js
@@ -1,4 +1,4 @@
 // components/SimpleText.js
 export default function SimpleText({ text }) {
-  return <p>{text}</p>;
+  return <h1>{text}</h1>;
 }
--- csicon-next-js/src/pages/index.js
+++ csicon-next-js/src/pages/index.js
@@ -24,6 +24,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <nav className="navbar navbar-expand-lg">
+        <header><h1>Header</h1></header>
+      </nav>
       <div
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable} container-fluid` }
       >
